209968af37328fb67a2070f81658da50c8536e43
Mahedi56 <shawon.mbstu.edu@gmail.com>
2025-03-18 14:49:33 +0600

    first commit in ringing bell

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+import QRCode from "react-qr-code";
+import RingBell from "./Components/RingBell";
+
+const baseURL = `http://${window.location.hostname}:3001/`;
+const qrCodeValue = `http://${window.location.hostname}:5173/?page=ringbell`;
+
+const App: React.FC = () => {
+  // const [ringBell, setRingBell] = useState(false);
+  
+  const params = new URLSearchParams(window.location.search);
+  const page = params.get("page");
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        const response = await fetch(`${baseURL}bellstatus`);  
+        const data = await response.json();
+        // setRingBell(data.isBellRung); 
+        console.log(data.isBellRung,data.count);
+        const playAudio = () => {
+          const audio = new Audio("ramadan.mp3"); // Replace with your audio file
+          audio.play().catch(error => console.log("Autoplay blocked:", error));
+        };
+        if(data.isBellRung&& data.count<=3){
+          playAudio();
+        }
+      } catch (error) {
+        console.error("Error fetching bell status:", error);
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (page === "ringbell") {
+    console.log("ringbell");
+    return <RingBell  />;
+  }
+
+  return (
+    <div className="flex flex-col items-center p-4">
+      <h1 className="text-2xl mb-4">Scan the QR Code</h1>
+      <QRCode value={qrCodeValue} size={200} />
+      <br />
+      {/* {ringBell && (
+        <div>
+          <h1>Audio: Ramadan</h1>
+          <audio controls autoPlay>
+            <source src="ramadan.mp3" type="audio/mp3" />
+            Audio is not supported.
+          </audio>
+        </div>
+      )} */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/RingBell.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+const baseURL = `http://${window.location.hostname}:3001/`;
+
+const RingBell: React.FC = () => {
+  const handleClick = async () => {
+    try {
+      const response = await fetch(`${baseURL}ringbell`, {  
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      });
+      const data = await response.json();
+      if (data.success) {
+        alert("Bell has been rung!");
+      }
+    } catch (error) {
+      console.error("Error ringing the bell:", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-xl mb-4">Click the bell button</h2>
+      <button 
+        onClick={handleClick} 
+        className="p-2 bg-blue-500 text-white rounded">
+        Ring Bell
+      </button>
+    </div>
+  );
+};
+
+export default RingBell;
